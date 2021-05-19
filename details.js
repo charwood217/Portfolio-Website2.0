022a6f4310a249d73345525a1b6a4704bde56e8d
Christian Harwood <charwood217@west-mec.org>
2021-05-19 15:26:39 -0700

    Final Touches Part 1

--- details.js
+++ details.js
@@ -4,10 +4,30 @@ function changeText(text)
             display.innerHTML = "";
             display.innerHTML = text;
         }
+
+
               
-        function defaultText()
+function defaultText()
         {
             var display = document.getElementById('details');
             display.innerHTML = "";
-            display.innerHTML = "here are all the skills and strengths I have developed in the last two years.";
+            display.innerHTML = "Here are all the Skills and Expieriences I have learned over the past 2 years";
         }
+       
+function changeHead(text)
+        {
+            var display = document.getElementById('head');
+            display.innerHTML = "";
+            display.innerHTML = text;
+        }
+
+function defaultHead(){
+    var display = document.getElementById('head');
+            display.innerHTML = "";
+            display.innerHTML = "Welcome";
+        }
+
+function hoverAlert(){
+    alert("Hover over the bars on the graph to get more details - ¢hri$")
+}
+
