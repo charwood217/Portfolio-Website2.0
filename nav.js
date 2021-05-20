2b9eae275447edbdc308fd314583e70b1c87df4a
Christian Harwood <charwood217@west-mec.org>
2021-05-19 18:58:23 -0700

    Made Site Mobile

---
+++ nav.js
@@ -0,0 +1,10 @@
+"use strict";
+/* Open when someone clicks on the span */
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the symbol */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
